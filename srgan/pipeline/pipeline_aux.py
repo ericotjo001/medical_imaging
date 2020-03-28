ac76ac3df0554a6820fc9c98c11360b9c5ca0339
etjoa003 <ericotjoa@gmail.com>
2020-03-28 16:37:55 +0800

    cleanup

--- srgan/pipeline/pipeline_aux.py
+++
@@ -1,5 +0,0 @@
-from utils.utils import *
-
-def training_cnn_print_data(x, labels):
-	print("  pipeline/pipeline_aux.py")
-	print("    x.shape = %s, labels.shape = %s"%(str(x.shape),str(labels.shape)))
